e28f41b04e3920a8120098bf7cc78ae2418315d0
Igor Kamyshev <garik.novel@gmail.com>
2017-06-08 22:39:01 +0700

    Добавлена сущность для поста

---
+++ src/AppBundle/Entity/Post.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ */
+class Post
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     * @ORM\Column(type="integer")
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text", nullable=false)
+     * @var string
+     */
+    private $text;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * @param string $text
+     * @return Post
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+        return $this;
+    }
+}
