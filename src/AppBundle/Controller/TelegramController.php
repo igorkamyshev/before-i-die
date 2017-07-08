850358e8d5a82f981c3146e01f952c9b3143933e
Igor Kamyshev <garik.novel@gmail.com>
2017-07-08 21:46:44 +0700

    Добавлены сущности для хранения чатов и контроллер для их сохранения

---
+++ src/AppBundle/Controller/TelegramController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AppBundle\Controller;
+
+
+use AppBundle\Entity\TelegramChat;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Telegram\Bot\Api;
+
+class TelegramController extends Controller
+{
+    /**
+     * @Route("/telegram/{apiKey}", name="telegram")
+     */
+    public function telegramAction($apiKey)
+    {
+        $telegramApiKey = $this->getParameter('app.telegram_api_key');
+
+        if ($apiKey != $telegramApiKey) {
+            throw $this->createAccessDeniedException();
+        }
+
+        $request = (new Api($telegramApiKey))->getWebhookUpdates();
+
+        $chatId = $request["message"]["chat"]["id"];
+
+        $chat = (new TelegramChat())->setChat($chatId);
+
+        $em = $this->getDoctrine()->getEntityManager();
+        $em->persist($chat);
+        $em->flush();
+
+        return new Response('ok');
+    }
+}
