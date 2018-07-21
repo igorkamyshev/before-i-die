b4c97e45bcec409186efedae8fa12fa3908bdfc1
Igor Kamyshev <garik.novel@gmail.com>
2018-07-21 17:49:44 +0300

    Telegram intergation

--- app/index.js
+++ app/index.js
@@ -1,10 +1,34 @@
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const express = require('express')
+const telegramBot = require('./telegram-bot')
 
 const app = express()
 const port = 8080
 
-app.get('/', (_, res) => {
-  res.send('Hello!')
-})
+app.use(bodyParser.json())
+app.use(cors())
 
+app.get('/message', (_, res) => telegramBot.getLastMessage()
+  .then(
+    (msg) => {
+      res.status(200)
+      res.send(msg)
+    },
+    (err) => {
+      res.status(500)
+      res.send(err)
+    },
+  ))
+
+app.post('/new', (req, res) => telegramBot.newPost(req.body.message)
+  .then(
+    () => res.status(200),
+    () => res.status(500),
+  )
+  .then(
+    () => res.send(),
+  ))
+
+telegramBot.start()
 app.listen(port)
---
+++ app/telegram-bot.js
@@ -0,0 +1,33 @@
+const TelegramBot = require('node-telegram-bot-api')
+const { TELEGRAM_BOT_TOKEN } = require('./env')
+
+const TARGET_CHANNEL = '@before_i_die'
+
+const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, { polling: true })
+
+const send = (chatId, message) => bot.sendMessage(chatId, message, { parse_mode: 'Markdown' })
+const publish = request => send(TARGET_CHANNEL, request.text)
+const response = (request, message) => send(request.chat.id, message)
+
+const start = () => bot.on('message', req => (
+  (req.text === '/start')
+    ? response(req, 'Привет. Отправь мне свои мысли, я анонимно опубликую их.')
+    : publish(req)
+))
+
+const getLastMessage = () => bot.getChat(TARGET_CHANNEL)
+  .then((chat) => {
+    if (!chat.pinned_message) {
+      throw Error('last message unavalible')
+    }
+
+    return chat.pinned_message.text
+  })
+
+const newPost = message => publish({ text: message })
+
+module.exports = {
+  start,
+  getLastMessage,
+  newPost,
+}
