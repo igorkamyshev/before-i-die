53d70b7f0fef98afbfe8ecd99d86d26dd0ea5db5
Igor Kamyshev <igor@kamyshev.me>
2018-07-21 17:34:41 +0300

    Added endpoint for new message

--- app/index.js
+++ app/index.js
@@ -1,8 +1,11 @@
+const bodyParser = require('body-parser')
 const express = require('express')
 const telegramBot = require('./telegram-bot')
 
 const app = express()
 const port = 8080
+
+app.use(bodyParser.json())
 
 app.get('/', (_, res) => {
   res.send('Hello!')
@@ -21,6 +24,16 @@ app.get('/message', (_, res) => {
         },
     )
 })
+
+app.post('/new', (req, res) => telegramBot.newPost(req.body.message)
+  .then(
+    () => res.status(200),
+    () => res.status(500),
+  )
+  .then(
+    () => res.send()
+  )
+)
 
 telegramBot.start()
 app.listen(port)
